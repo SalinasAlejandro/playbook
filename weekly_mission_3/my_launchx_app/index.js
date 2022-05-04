45a1e868998ce7d378f53bdb865be60cbb36fdc3
SalinasAlejandro <dykeo.alejandro@gmail.com>
2022-05-03 23:37:07 -0500

    Realización del ejercicio 1. Creación de proyectos de JS de la semana 3

---
+++ weekly_mission_3/my_launchx_app/index.js
@@ -0,0 +1,7 @@
+const MissionCommander = require('./app/missionCommander');
+const mc1 = new MissionCommander("Dykeo");
+const mc2 = new MissionCommander("Alejandro");
+const mc3 = new MissionCommander("Salinas");
+console.log(mc1.name);
+console.log(mc2.name);
+console.log(mc3.name);
